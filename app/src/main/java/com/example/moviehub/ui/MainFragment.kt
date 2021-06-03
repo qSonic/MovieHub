8b967410383ae4deabd311b669772e245ab0cb29
qSonic <bashkin415@gmail.com>
2021-06-03 14:05:36 +0300

    Initial commit

--- app/src/main/java/com/example/moviehub/ui/MainFragment.kt
+++ app/src/main/java/com/example/moviehub/ui/MainFragment.kt
@@ -1,14 +1,15 @@
 package com.example.moviehub.ui
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.ViewModelProvider
 import com.example.moviehub.R
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), LifecycleObserver {
 
     companion object {
         fun newInstance() = MainFragment()
@@ -16,15 +17,12 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
 }
